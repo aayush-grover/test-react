dfec5ca58ff6e4092c878ce2fe42037908a76a80
aayush-grover <aayushgrover3@gmail.com>
2019-12-09 14:43:59 +0530

    Updates

---
+++ precache-manifest.7897824d51890ca593dd9c97d0d0c175.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84493f16917f87f63408fb6c35c3845",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "1606e48fa121ae0aa05c",
+    "url": "/test-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f2a49d045aa3a0cac43a",
+    "url": "/test-react/static/js/2.2b1f4b48.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test-react/static/js/2.2b1f4b48.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1606e48fa121ae0aa05c",
+    "url": "/test-react/static/js/main.b4b5426a.chunk.js"
+  },
+  {
+    "revision": "5b12e971e821f3bf2b02",
+    "url": "/test-react/static/js/runtime-main.e356ded1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
